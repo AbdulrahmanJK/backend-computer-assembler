cd28d560d9a7b4b89ca6e84d09e1424a8d8da9e9
Abuyusuf99 <mus.korol@mail.ru>
2023-09-19 16:16:49 +0300

    This is a new commit for what I originally planned to be an amendmend

--- routers/accessories.route.js
+++ routers/accessories.route.js
@@ -5,8 +5,9 @@ const router = Router()
 
 router.post("/accessories", accessoriesControllers.createAccessories)
 router.get("/accessories", accessoriesControllers.getAccessories)
-router.get("/accessories/:id", accessoriesControllers.getOneAccessory)
+router.get("/accessories/one/:id", accessoriesControllers.getOneAccessory)
 router.delete("/accessories/:id", accessoriesControllers.deleteAccessories)
+router.get("/accessories/:id", accessoriesControllers.getAllCategories)
 router.patch("/accessories/:id", accessoriesControllers.patchAccessories)
 
 module.exports = router
